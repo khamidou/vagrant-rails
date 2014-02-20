11992c60dd8b11ffe7400ba864247fef278b8fbd
karim hamidou <khamidou@gmail.com>
2014-02-20 19:09:42 +0100

    forgot nodejs - required for asset pipeline

--- provision.sh
+++ provision.sh
@@ -3,4 +3,10 @@
 apt-get update
 apt-get -y install curl git build-essential libsqlite3-dev
 
+# install nodejs - apparently a requirement for the asset pipeline
+apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
 sudo -Hu vagrant /bin/bash /vagrant/setup_rails.sh
